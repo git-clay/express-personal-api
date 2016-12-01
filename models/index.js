edbe79b4071d1862e9ab68c23d26828ce6b8dc46
git-clay <ce.smith5525@gmail.com>
2016-12-01 15:29:44 -0700

    start

--- models/index.js
+++ models/index.js
@@ -3,5 +3,4 @@ mongoose.connect( process.env.MONGODB_URI ||
                   process.env.MONGOLAB_URI || 
                   process.env.MONGOHQ_URL || 
                   "mongodb://localhost/personal-api");
-
 // module.exports.Campsite = require("./campsite.js.example");
