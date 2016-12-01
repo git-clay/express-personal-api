fe3875f7d70b22e12b69446dd71df6dc3ba8cff0
git-clay <ce.smith5525@gmail.com>
2016-12-01 16:16:24 -0700

    push to heroku

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/api', function api_index(req, res) {
   res.json({
     woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
+    documentation_url: "https://github.com/git-clay/express-personal-api/README.md", // CHANGE ME
     base_url: "https://tranquil-mountain-12063.herokuapp.com/", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
